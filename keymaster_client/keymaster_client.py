e21ff43f94e75ca5e4d8385b4c1a461421322208
Adam Pickering <adamkpickering@gmail.com>
2020-07-21 19:08:14 +0000

    Committing changes in case I come back to this

--- keymaster_client/keymaster_client.py
+++ keymaster_client/keymaster_client.py
@@ -3,55 +3,28 @@ import time
 import logging
 
 import keymaster_client.wireguard as wg
-from keymaster_client.keymaster_api import KeymasterAPI
+from keymaster_client.config_source import uDPUAPI
 from keymaster_client.config_scheme import ConfigScheme
 
 
 LOGGER = logging.getLogger('keymaster_client')
 
 
-def parse_from_upstream(config: dict) -> dict:
-    """Takes the config that keymaster-server returns and puts
-    it into a format that can be consumed by WireguardInterface.from_dict."""
-    # peers
-    peers = []
-    for peer in config['peers']:
-        if peer.get('public_key') is not None:
-            output_peer = {
-                'public_key': peer['public_key'],
-                'allowed_ips': peer['allowed_ips'],
-                'endpoint': peer.get('endpoint'),
-                'persistent_keepalive': peer.get('persistent_keepalive')
-            }
-            peers.append(output_peer)
-
-    # interface
-    output_config = {
-        'name': config['interface']['name'],
-        'addresses': config['interface']['addresses'],
-        'listen_port': config['interface'].get('listen_port'),
-        'peers': peers
-    }
-
-    return output_config
-
-
-def configure_wireguard_interface(server: KeymasterAPI, config_scheme: ConfigScheme,
-                                  wg_config: dict, private_key: str = None):
+def configure_wireguard_interface(config_source: uDPUAPI, config_scheme: ConfigScheme,
+                                  desired_interface: wg.WireguardInterface, private_key: str = None):
     """An idempotent function that compares the current configured interface
-    (if previously configured) to the one represented by `wg_config`, which
-    is meant to be received from keymaster-server. Always leaves the interface
-    in the state described by `wg_config`, with one exception: when `private_key`
-    is defined. In this case, `private_key` takes precedence over any other
-    private key present. This is so that keymaster-client can be made to
-    have the same configuration on multiple servers.
+    (if previously configured) to the one represented by `desired_interface`.
+    Always leaves the interface in the state described by `desired_interface`,
+    with one exception: when `private_key` is defined. In this case, `private_key`
+    takes precedence over any other private key present. This is so that
+    a number of hosts running keymaster-client can be made to have the same
+    configuration (for redundant wireguard configs).
 
     An addition purpose of `configure_wireguard_interface` is to notify
     keymaster-server of any changes to the public key for the interface
     it configures."""
-    api_interface_id = wg_config['interface']['_id']
     interface_name = wg_config['interface']['name']
-    api_public_key = wg_config['interface'].get('public_key')
+    desired_public_key = wg_config['interface'].get('public_key')
 
     if config_scheme.interface_exists(interface_name):
 
@@ -69,7 +42,7 @@ def configure_wireguard_interface(server: KeymasterAPI, config_scheme: ConfigSch
 
         current_public_key = wg.get_public_key(api_interface.private_key)
         if api_public_key != current_public_key:
-            server.patch_server_public_key(api_interface_id, current_public_key)
+            config_source.patch_public_key(desired_interface.name, current_public_key)
 
     else:
         formatted_api_config = parse_from_upstream(wg_config)
@@ -82,7 +55,7 @@ def configure_wireguard_interface(server: KeymasterAPI, config_scheme: ConfigSch
         config_scheme.write(new_interface)
         LOGGER.debug(f'interface {interface_name}: interface configured')
         new_public_key = wg.get_public_key(new_interface.private_key)
-        server.patch_server_public_key(api_interface_id, new_public_key)
+        config_source.patch_public_key(api_interface_id, new_public_key)
         LOGGER.debug(f'interface {interface_name}: public key uploaded')
 
 
