d6aea4805eef614cee7f7073cf3e901529f4f553
Shaun Martin <shaun@samsite.ca>
2020-07-27 23:08:24 +0000

    [gitbot] autobump: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='test',
     tests_require=test_requirements,
     url='https://github.com/telus/keymaster-client',
-    version='0.0.0',
+    version='0.0.1',
     zip_safe=False,
 )
