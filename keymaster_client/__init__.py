0e2572a8514113a8b73d7b90b0035cf0ffaaaa38
Shaun Martin <shaun@samsite.ca>
2020-09-08 21:23:47 +0000

    [gitbot] autobump: 0.0.6 → 0.0.7

--- keymaster_client/__init__.py
+++ keymaster_client/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Adam Pickering'
 __email__ = 'adamkpickering@gmail.com'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 from keymaster_client.config_scheme import wgConfigScheme, UCIConfigScheme
 from keymaster_client.wireguard import (
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='test',
     tests_require=test_requirements,
     url='https://github.com/telus/keymaster-client',
-    version='0.0.6',
+    version='0.0.7',
     zip_safe=False,
 )
