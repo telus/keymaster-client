4a995746746c234e787adc4fd64c7946ffaec9cc
Shaun Martin <shaun@samsite.ca>
2020-10-21 18:06:17 +0000

    [gitbot] autobump: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements_dev.txt') as devreqs_file:
 
 setup(
     name='keymaster_client',
-    version='1.0.1',
+    version='1.0.2',
     author='Adam Pickering',
     author_email='adamkpickering@gmail.com',
     description="Configures wireguard using information received from keymaster-server",
